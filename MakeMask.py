fe924fe3c780b9a2f5b7284caf4f298569c1c09a
Christopher Añorve <canorve@gmail.com>
2018-12-17 11:27:34 -0700

    New files added to GALFITools  Changes to be committed: 	new file:   MakeMask.py 	new file:   mge2galfit.py 	new file:   xy2fits.py

---
+++ MakeMask.py
@@ -0,0 +1,544 @@
+#! /usr/bin/env python
+
+import numpy as np
+import sys
+import os
+import subprocess as sp
+from astropy.io import fits
+import os.path
+import scipy
+
+# This program creates a catalog of Sextractor with
+# a combination of two runs of Sextractor with
+# different configuration parameters
+
+def main():
+
+    if len(sys.argv[1:]) != 4:
+        print ('Missing arguments')
+        print ("Usage:\n %s [SexFile] [ImageFile] [MaskFileOut] [scale]" % sys.argv[0])
+        print ("Example:\n %s sex.cat image.fits mask.fits 1.5" % sys.argv[0])
+        sys.exit()
+
+    sexfile= sys.argv[1]
+    image= sys.argv[2]
+    maskfile= sys.argv[3]
+    scale= sys.argv[4]
+
+    scale = float(scale)
+
+
+    sexarsort   = "sexsort.cat"
+    satfileout  = "ds9sat.reg"
+
+    satscale = 1
+    satoffset = 0
+
+
+    print ("Creating masks....\n")
+
+    (NCol, NRow) = GetAxis(image)
+
+    Total = CatArSort(sexfile,scale,sexarsort,NCol,NRow)
+
+    print ("Creating sat region files....\n")
+
+    ds9satbox(satfileout,sexfile,satscale,satoffset) # crea archivo  Saturacion reg
+
+
+##### segmentation mask
+
+    MakeImage(maskfile, NCol, NRow)
+
+    MakeMask(maskfile, sexarsort, scale, 0, satfileout)  # offset set to 0
+    MakeSatBox(maskfile, satfileout, Total + 1, NCol, NRow) #make sat region
+
+
+
+def ds9satbox (satfileout,output,satscale,satoffset):
+    "Creates a file for ds9 which selects bad saturated regions"
+
+    scaleflag=1
+    offsetflag=1
+    regfileflag=1
+    magflag=1
+    clasflag=1
+
+    flagsat=4      ## flag value when object is saturated (or close to)
+    maxflag=128    ## max value for flag
+    check=0
+    regflag = 0    ## flag for saturaded regions
+
+
+
+
+    f_out = open(satfileout, "w")
+
+    N,Alpha,Delta,X,Y,Mg,Kr,Fluxr,Isoa,Ai,E,Theta,Bkgd,Idx,Flg=np.genfromtxt(output,delimiter="",unpack=True)
+
+
+    line="image \n"
+    f_out.write(line)
+
+
+
+    for idx, item in enumerate(N):
+
+
+        bi=Ai[idx]*(1-E[idx])
+
+        Theta[idx] = Theta[idx] * np.pi /180  #rads!!!
+
+
+        Rkronx = satscale * 2 * Ai[idx] * Kr[idx]  + satoffset
+        Rkrony = satscale * 2 * bi * Kr[idx]  + satoffset
+
+
+
+        if Rkronx == 0:
+            Rkronx = 1
+
+        if Rkrony == 0:
+            Rkrony = 1
+
+        check=CheckFlag(Flg[idx],flagsat,maxflag)  ## check if object has saturated regions
+
+        if (check):
+
+            line="box({0},{1},{2},{3},0) # color=red move=0 \n".format(X[idx],Y[idx],Rkronx,Rkrony)
+            f_out.write(line)
+
+            line2="point({0},{1}) # point=boxcircle font=\"times 10 bold\" text={{ {2} }} \n".format(X[idx],Y[idx],N[idx])
+            f_out.write(line2)
+
+
+    f_out.close()
+
+
+
+
+
+def MakeMask(maskimage, catfile, scale, offset, regfile):
+    "Create a mask image using ellipses for every Object of catfile. Now includes offset"
+# k Check
+
+    checkflag = 0
+    flagsat = 4  # flag value when object is saturated (or close to)
+    maxflag = 128  # max value for flag
+
+    regflag = 0  # flag for saturaded regions
+
+    n, alpha, delta, xx, yy, mg, kr, fluxrad, ia, ai, e, theta, bkgd, idx, flg, sxmin, sxmax, symin, symax, sxsmin, sxsmax, sysmin, sysmax = np.genfromtxt(
+        catfile, delimiter="", unpack=True)
+
+    n = n.astype(int)
+    flg = flg.astype(int)
+
+    print("Creating Masks for sky \n")
+
+    Rkron = scale * ai * kr + offset
+
+    mask = Rkron < 1
+    if mask.any():
+        Rkron[mask] = 1
+
+    hdu = fits.open(maskimage)
+    img = hdu[0].data
+
+    for idx, val in enumerate(n):
+
+        # check if object doesn't has saturaded regions
+        checkflag = CheckFlag(flg[idx], flagsat, maxflag)
+        # check if object is inside of a saturaded box region indicated by
+        # user in ds9
+        regflag = CheckSatReg(xx[idx], yy[idx], regfile, Rkron[idx], theta[idx], e[idx])
+
+        if (checkflag == False) and (regflag == False):
+
+            print ("Creating ellipse mask for object {}  \n".format(n[idx]))
+            img = MakeKron(img, n[idx], xx[idx], yy[idx], Rkron[idx], theta[idx], e[
+                idx], sxsmin[idx], sxsmax[idx], sysmin[idx], sysmax[idx])
+
+        elif(checkflag == True or regflag == True):
+
+            print ("Skipping object {}, one or more pixels are saturated \n".format(n[idx]))
+
+    hdu[0].data = img
+    hdu.writeto(maskimage, overwrite=True)
+    hdu.close()
+
+    return True
+
+
+def MakeKron(imagemat, idn, x, y, R, theta, ell, xmin, xmax, ymin, ymax):
+    "This subroutine create a Kron ellipse within a box defined by: xmin, xmax, ymin, ymax"
+
+# Check
+
+    xmin = np.int(xmin)
+    xmax = np.int(xmax)
+    ymin = np.int(ymin)
+    ymax = np.int(ymax)
+
+    q = (1 - ell)
+    bim = q * R
+
+    theta = theta * np.pi / 180  # Rads!!!
+
+    ypos, xpos = np.mgrid[ymin - 1:ymax, xmin - 1:xmax]
+
+    dx = xpos - x
+    dy = ypos - y
+
+    landa = np.arctan2(dy, dx)
+
+    mask = landa < 0
+    if mask.any():
+        landa[mask] = landa[mask] + 2 * np.pi
+
+    landa = landa - theta
+
+    angle = np.arctan2(np.sin(landa) / bim, np.cos(landa) / R)
+
+    xell = x + R * np.cos(angle) * np.cos(theta) - bim * \
+        np.sin(angle) * np.sin(theta)
+    yell = y + R * np.cos(angle) * np.sin(theta) + bim * \
+        np.sin(angle) * np.cos(theta)
+
+    dell = np.sqrt((xell - x)**2 + (yell - y)**2)
+    dist = np.sqrt(dx**2 + dy**2)
+
+    mask = dist < dell
+    imagemat[ypos[mask], xpos[mask]] = idn
+
+    return imagemat
+
+
+def MakeSatBox(maskimage, region, val, ncol, nrow):
+    "Create a mask for saturated regions"
+    "Regions must be in DS9 box regions format"
+
+# k Check
+
+#	fileflag=1
+
+    hdu = fits.open(maskimage)
+    img = hdu[0].data
+
+    with open(region) as f_in:
+
+        next(f_in)
+#        next(f_in)
+#        next(f_in)
+
+        # All lines including the blank ones
+        lines = (line.rstrip() for line in f_in)
+        lines = (line.split('#', 1)[0] for line in lines)  # remove comments
+        # remove lines containing only comments
+        lines = (line.rstrip() for line in lines)
+        lines = (line for line in lines if line)  # Non-blank lines
+
+        for line in lines:
+
+            (box, info) = line.split('(')
+
+            if(box == "box"):
+
+                (xpos, ypos, xlong, ylong, trash) = info.split(',')
+
+                xpos = float(xpos)
+                ypos = float(ypos)
+                xlong = float(xlong)
+                ylong = float(ylong)
+
+                xlo = (xpos - xlong / 2)
+                xhi = (xpos + xlong / 2)
+
+                ylo = (ypos - ylong / 2)
+                yhi = (ypos + ylong / 2)
+
+                xlo = int(xlo)
+                xhi = int(xhi)
+
+                ylo = int(ylo)
+                yhi = int(yhi)
+
+                if (xlo < 1):
+
+                    xlo = 1
+
+                if (xhi > ncol):
+
+                    xhi = ncol
+
+                if (ylo < 1):
+
+                    ylo = 1
+
+                if (yhi > nrow):
+
+                    yhi = nrow
+
+                img[ylo - 1:yhi, xlo - 1:xhi] = val
+
+    hdu[0].data = img
+    hdu.writeto(maskimage, overwrite=True)
+    hdu.close()
+
+    return True
+
+
+
+
+def MakeImage(newfits, sizex, sizey):
+    "create a new blank Image"
+# k Check
+    if os.path.isfile(newfits):
+        print("{} deleted; a new one is created \n".format(newfits))
+    hdu = fits.PrimaryHDU()
+    hdu.data = np.zeros((sizey, sizex))
+    hdu.writeto(newfits, overwrite=True)
+
+    return True
+
+
+def GetAxis(Image):
+    # k Check
+    "Get number of rows and columns from the image"
+
+    hdu = fits.open(Image)
+    ncol = hdu[0].header["NAXIS1"]
+    nrow = hdu[0].header["NAXIS2"]
+    hdu.close()
+    return ncol, nrow
+
+
+def CatArSort(SexCat,scale,SexArSort,NCol,NRow):
+    # k Check
+
+    # sort the sextractor
+    # catalog by magnitude,
+    # get sizes for objects
+    # and write it in a new file
+
+    # The sextractor catalog must contain the following parameters:
+    #   1 NUMBER                 Running object number
+    #   2 ALPHA_J2000            Right ascension of barycenter (J2000)                      [deg]
+    #   3 DELTA_J2000            Declination of barycenter (J2000)                          [deg]
+    #   4 X_IMAGE                Object position along x                                    [pixel]
+    #   5 Y_IMAGE                Object position along y                                    [pixel]
+    #   6 MAG_APER               Fixed aperture magnitude vector                            [mag]
+    #   7 KRON_RADIUS            Kron apertures in units of A or B
+    #   8 FLUX_RADIUS            Fraction-of-light radii                                    [pixel]
+    #   9 ISOAREA_IMAGE          Isophotal area above Analysis threshold                    [pixel**2]
+    #  10 A_IMAGE                Profile RMS along major axis                               [pixel]
+    #  11 ELLIPTICITY            1 - B_IMAGE/A_IMAGE
+    #  12 THETA_IMAGE            Position angle (CCW/x)                                     [deg]
+    #  13 BACKGROUND             Background at centroid position                            [count]
+    #  14 CLASS_STAR             S/G classifier output
+    #  15 FLAGS                  Extraction flags
+
+
+    print("Sorting and getting sizes for objects \n")
+
+    n, alpha, delta, xx, yy, mg, kr, fluxrad, ia, ai, e, theta, bkgd, idx, flg = np.genfromtxt(
+        SexCat, delimiter="", unpack=True)
+
+    n = n.astype(int)
+    flg = flg.astype(int)
+
+#    ai = ai.astype(float)
+#    kr = kr.astype(float)
+
+#    scale = scale.astype(float)
+
+
+    Rkron = scale * ai * kr
+
+    Rwsky = scale * ai * kr + 10  + 20
+
+
+#   considering to use only  KronScale instead of SkyScale
+#    Rwsky = parvar.KronScale * ai * kr + parvar.Offset + parvar.SkyWidth
+
+    Bim = (1 - e) * Rkron
+
+    Area = np.pi * Rkron * Bim *(-1)
+
+    (sxmin, sxmax, symin, symax) = GetSize(xx, yy, Rkron, theta, e, NCol, NRow)
+
+    (sxsmin, sxsmax, sysmin, sysmax) = GetSize(xx, yy, Rwsky, theta, e, NCol,NRow)
+
+
+    f_out = open(SexArSort, "w")
+
+    index = Area.argsort()
+    for i in index:
+
+        line = "{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}\n".format(n[i], alpha[i], delta[i], xx[i], yy[i], mg[i], kr[i], fluxrad[i], ia[i], ai[i], e[i], theta[i], bkgd[i], idx[i], flg[i], np.int(
+            np.round(sxmin[i])), np.int(np.round(sxmax[i])), np.int(np.round(symin[i])), np.int(np.round(symax[i])), np.int(np.round(sxsmin[i])), np.int(np.round(sxsmax[i])), np.int(np.round(sysmin[i])), np.int(np.round(sysmax[i])))
+
+        f_out.write(line)
+
+    f_out.close()
+
+    return len(n)
+
+
+def GetSize(x, y, R, theta, ell, ncol, nrow):
+    "this subroutine get the maximun"
+    "and minimim pixels for Kron and sky ellipse"
+    # k Check
+    q = (1 - ell)
+    bim = q * R
+
+    theta = theta * (np.pi / 180)  # rads!!
+
+# getting size
+
+    xmin = x - np.sqrt((R**2) * (np.cos(theta))**2 +
+                       (bim**2) * (np.sin(theta))**2)
+
+    xmax = x + np.sqrt((R**2) * (np.cos(theta))**2 +
+                       (bim**2) * (np.sin(theta))**2)
+
+    ymin = y - np.sqrt((R**2) * (np.sin(theta))**2 +
+                       (bim**2) * (np.cos(theta))**2)
+
+    ymax = y + np.sqrt((R**2) * (np.sin(theta))**2 +
+                       (bim**2) * (np.cos(theta))**2)
+
+    mask = xmin < 1
+    if mask.any():
+        xmin = 1
+
+    mask = xmax > ncol
+
+    if mask.any():
+        xmax = ncol
+
+    mask = ymin < 1
+    if mask.any():
+        ymin = 1
+
+    mask = ymax > nrow
+    if mask.any():
+        ymax = nrow
+
+    return (xmin, xmax, ymin, ymax)
+
+def CheckFlag(val,check,max):
+   "Check for flag contained in $val, returns 1 if found "
+
+   flag = False
+   mod = 1
+
+   while (mod != 0):
+
+
+       res = int(val/max)
+
+       if (max == check and res == 1 ):
+
+           flag=True
+
+
+       mod = val % max
+
+       val = mod
+       max = max/2
+
+
+
+   return flag
+
+
+def CheckSatReg(x,y,filein,R,theta,ell):
+   "Check if object is inside of saturated region. returns True if at least one pixel is inside"
+## check if object is inside of
+## saturaded region as indicated by ds9 box region
+## returns 1 if object center is in saturaded region
+
+
+   q = (1 - ell)
+
+   bim = q * R
+
+   theta = theta * np.pi /180  ## Rads!!!
+
+   flag = False
+   fileflag =1
+
+
+
+   with open(filein) as f_in:
+
+       lines = (line.rstrip() for line in f_in) # All lines including the blank ones
+       lines = (line.split('#', 1)[0] for line in lines) # remove comments
+       lines = (line.rstrip() for line in lines)   # remove lines containing only comments
+       lines = (line for line in lines if line) # Non-blank lines
+
+       for line in lines:
+
+
+           if (line != "image"):
+
+               (box,info)=line.split('(')
+
+               if(box == "box"):
+
+                   (xpos,ypos,xlong,ylong,trash)=info.split(',')
+
+                   xpos=float(xpos)
+                   ypos=float(ypos)
+                   xlong=float(xlong)
+                   ylong=float(ylong)
+
+
+                   xlo = xpos - xlong/2
+                   xhi = xpos + xlong/2
+
+                   ylo = ypos - ylong/2
+                   yhi = ypos + ylong/2
+
+                   dx = xpos - x
+                   dy = ypos - y
+
+                   landa=np.arctan2( dy,dx )
+
+                   if landa < 0:
+                       landa=landa + 2 * np.pi
+
+
+                   landa = landa - theta
+
+                   angle = np.arctan2(np.sin(landa)/bim, np.cos(landa)/R)
+
+                   xell =  x + R * np.cos(angle)* np.cos(theta)  - bim * np.sin(angle) * np.sin(theta)
+                   yell =  y + R * np.cos(angle)* np.sin(theta)  + bim * np.sin(angle) * np.cos(theta)
+
+
+                   if ( (xell > xlo and xell < xhi) and (yell > ylo and yell < yhi)  ):
+
+                       flag=True
+                       break
+
+
+   return flag
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#end of program
+if __name__ == '__main__':
+    main()
