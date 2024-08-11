f00f62a1bdf6b466947dc9eb79f35511305c34d1
Christopher Añorve <canorve@gmail.com>
2024-08-10 19:51:26 -0700

    modified version of print outputs  Changes to be committed: 	modified:   src/galfitools/galout/getBarSize.py

--- src/galfitools/galout/getBarSize.py
+++ src/galfitools/galout/getBarSize.py
@@ -72,9 +72,10 @@ def getBarSize(
 
     line = "# Region file format: DS9 version 4.1 \n"
     fout.write(line)
-    line = 'global color=red dashlist=8 3 width=2 '
-    + 'font="helvetica 10 normal roman" select=1 highlite=1 dash=0 '
-    + 'fixed=0 edit=1 move=1 delete=1 include=1 source=1\n'
+    linea = "global color=red dashlist=8 3 width=2 "
+    lineb = 'font="helvetica 10 normal roman" select=1 highlite=1 dash=0 '
+    linec = "fixed=0 edit=1 move=1 delete=1 include=1 source=1\n"
+    line = linea + lineb + linec
     fout.write(line)
     line = "physical\n"
     fout.write(line)
