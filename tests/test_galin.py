6714a8fd16566889606ffa94b3e65a042e6e7f9c
Christopher Añorve <canorve@gmail.com>
2024-08-31 23:17:14 -0700

    checkfile test added  Changes to be committed: 	new file:   fit.log 	modified:   test_galin.py

--- tests/test_galin.py
+++ tests/test_galin.py
@@ -1,11 +1,11 @@
-
-
 import pytest
 import os
 
 
 import subprocess as sp
+import numpy as np
 
+from collections import Counter
 
 
 from galfitools.galin.getStar import getStar
@@ -17,38 +17,36 @@ from galfitools.galin.MaskSky import skyRem
 from galfitools.galin.xy2fits import xy2fits
 
 
+from galfitools.galin.checkGalFile import checkFile
+
 
 def test_getStar():
-
 
     image = "A671.gtMakeMask.maskds9.masksky.fits"
     regfile = "ds9.getStar.reg"
 
-    path="tests/"
+    path = "tests/"
 
-    image = path+image
-    regfile = path+regfile
+    image = path + image
+    regfile = path + regfile
 
-    imsize = 70 
-    center = False 
+    imsize = 70
+    center = False
 
-    sky = 1153 
-    imout = "testgetStar.fits" 
+    sky = 1153
+    imout = "testgetStar.fits"
 
-    imout = path+imout
+    imout = path + imout
 
-    sigma = None 
-    sigout = None 
+    sigma = None
+    sigout = None
 
     getStar(image, regfile, imsize, center, sky, imout, sigma, sigout)
-
 
     assert os.path.isfile(imout)
 
     if os.path.isfile(imout):
         os.remove(imout)
-
-
 
     return None
 
@@ -57,78 +55,75 @@ def test_InitGal():
 
     GalfitFile = "galfit.initgal"
 
+    path = "tests/"
+    GalfitFile = path + GalfitFile
 
-    path="tests/"
-    GalfitFile=path+GalfitFile
-
-    number = 1 
-    param3 = [1,50] 
-    param4 = None 
-    param5 = None 
-    param6 = None 
-    param7 = None 
-    param8 = None 
-    param9 = None 
-    param10 = None 
-
+    number = 1
+    param3 = [1, 50]
+    param4 = None
+    param5 = None
+    param6 = None
+    param7 = None
+    param8 = None
+    param9 = None
+    param10 = None
 
     numcomp = 1
 
+    InitGal(
+        GalfitFile,
+        number,
+        param3,
+        param4,
+        param5,
+        param6,
+        param7,
+        param8,
+        param9,
+        param10,
+        numcomp,
+    )
 
-    InitGal(GalfitFile, number, param3, param4, param5, param6, param7, param8, param9, param10, numcomp)
+    fileout = "galfit-1.gal"
 
-
-    fileout="galfit-1.gal"
-
-    fileout = path+fileout
+    fileout = path + fileout
 
     assert os.path.isfile(fileout)
 
     if os.path.isfile(fileout):
         os.remove(fileout)
 
-
     namefile = "rungalfit.sh"
-    #namefile = path+namefile
+    # namefile = path+namefile
 
     assert os.path.isfile(namefile)
     if os.path.isfile(namefile):
         os.remove(namefile)
 
-
-
-
-
-
     return None
-
 
 
 def test_maskDs9():
 
-
-    MaskFile = "tempmask.fits" 
-    RegFile = "maskds9.reg" 
-    fill = 100 
+    MaskFile = "tempmask.fits"
+    RegFile = "maskds9.reg"
+    fill = 100
     image = "A671.gtMakeMask.maskds9.masksky.fits"
 
-    
-    path="tests/"
-    MaskFile = path+MaskFile
+    path = "tests/"
+    MaskFile = path + MaskFile
     RegFile = path + RegFile
-    image = path + image 
+    image = path + image
 
-    bor_flag = False 
-    borValue = 100 
+    bor_flag = False
+    borValue = 100
 
-
-    maskDs9(MaskFile, RegFile, fill, image, bor_flag, borValue) 
+    maskDs9(MaskFile, RegFile, fill, image, bor_flag, borValue)
 
     assert os.path.isfile(MaskFile)
 
     if os.path.isfile(MaskFile):
         os.remove(MaskFile)
-
 
     return None
 
@@ -136,16 +131,15 @@ def test_maskDs9():
 def test_makeMask():
 
     sexfile = "cold.gtMakeMask"
-    image =  "A671.gtMakeMask.maskds9.masksky.fits"
-    maskfile = "tempmakemask.fits" 
-    scale = 1 
-    satfileout = "ds9sat.reg" 
+    image = "A671.gtMakeMask.maskds9.masksky.fits"
+    maskfile = "tempmakemask.fits"
+    scale = 1
+    satfileout = "ds9sat.reg"
 
-    path="tests/"
-    sexfile = path+sexfile
-    image=path+image
-    maskfile = path+maskfile
-
+    path = "tests/"
+    sexfile = path + sexfile
+    image = path + image
+    maskfile = path + maskfile
 
     makeMask(sexfile, image, maskfile, scale, satfileout)
 
@@ -155,41 +149,34 @@ def test_makeMask():
         os.remove(maskfile)
 
     namefile = "sexsort.cat"
-    #namefile = path+namefile
+    # namefile = path+namefile
 
     assert os.path.isfile(namefile)
     if os.path.isfile(namefile):
         os.remove(namefile)
 
-
-
     assert os.path.isfile(satfileout)
     if os.path.isfile(satfileout):
         os.remove(satfileout)
 
-
-
     return None
-
 
 
 def test_skyRem():
 
+    image = "A671.gtMakeMask.maskds9.masksky.fits"
+    mask = "tempmasksky.fits"
 
-    image =  "A671.gtMakeMask.maskds9.masksky.fits"
-    mask = "tempmasksky.fits" 
-
-    path="tests/"
-    image  = path+image 
+    path = "tests/"
+    image = path + image
     mask = path + mask
 
     sky_mean = 1150
     sky_sig = 14
-    nsig = 1 
+    nsig = 1
 
-    bor_flag = False 
-    borValue = 100 
-
+    bor_flag = False
+    borValue = 100
 
     skyRem(image, mask, sky_mean, sky_sig, nsig, borValue, bor_flag)
 
@@ -198,42 +185,76 @@ def test_skyRem():
     if os.path.isfile(mask):
         os.remove(mask)
 
-
-
     return None
+
 
 def test_xy2fits():
 
+    ImageFile = "A671.gtMakeMask.maskds9.masksky.fits"
 
-    ImageFile=  "A671.gtMakeMask.maskds9.masksky.fits"
+    AsciiFile = "maskscii.txt"
 
-    AsciiFile= "maskscii.txt" 
+    path = "tests/"
+    ImageFile = path + ImageFile
+    AsciiFile = path + AsciiFile
 
-    path="tests/"
-    ImageFile=path+ImageFile
-    AsciiFile=path+AsciiFile
-
-    Value = 100 
+    Value = 100
 
     xy2fits().MakeFits(ImageFile, AsciiFile, Value)
 
     maskfile = "maskscii.fits"
-    maskfile=path+maskfile
+    maskfile = path + maskfile
 
     assert os.path.isfile(maskfile)
 
     if os.path.isfile(maskfile):
         os.remove(maskfile)
 
-
-
     return None
 
 
+def test_checkfile():
 
+    path = "tests/"
 
+    galfitFile = "galfit.3ser"
+    galfitFile = path + galfitFile
+    dis = 10
 
+    headinfo, galax, mag, freepar = checkFile(galfitFile, dis)
 
+    tol = 1e-2
 
+    diffgal = abs(len(galax) - 3)
 
+    assert diffgal < tol
 
+    difftolgal = abs(len(np.unique(galax)) - 1)
+
+    assert difftolgal < tol
+
+    diffree = abs(freepar - 18)
+
+    assert diffree < tol
+
+    Flux = 10 ** (
+        (25 - mag) / 2.5
+    )  # 25 is just a constant to avoid small numbers. I substract it later
+
+    cnt = Counter(galax)
+
+    for idx, item in enumerate(np.unique(galax)):
+        totcomp = cnt[item]
+        maskgal = galax == item
+
+        totFlux = Flux[maskgal].sum()
+
+        totmag = -2.5 * np.log10(totFlux) + 25
+
+    diffcomp = abs(totcomp - 3)
+    assert diffcomp < tol
+
+    diffmag = abs(totmag - 11.26)
+    assert diffmag < tol
+
+    return None
