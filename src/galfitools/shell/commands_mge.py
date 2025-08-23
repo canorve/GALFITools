60492faf8d2a82e1c6fb156003da26d50d9d37b2
Christopher Añorve <canorve@gmail.com>
2025-08-22 22:48:34 -0700

    mge reformatted for tests

--- src/galfitools/shell/commands_mge.py
+++ src/galfitools/shell/commands_mge.py
@@ -5,6 +5,73 @@ from galfitools.mge.SbProf import sbProf
 from galfitools.shell.prt import printWelcome
 
 
+# ---- helpers (small, testable) ----
+def _build_parser_mge() -> argparse.ArgumentParser:
+    p = argparse.ArgumentParser(
+        description="fits mge of Cappellari and formats to GALFIT"
+    )
+    p.add_argument("GalfitFile", help="GALFIT file to obtain the header options")
+    p.add_argument(
+        "Ds9regFile",
+        help=(
+            "the DS9 ellipse region file containing the galaxy. "
+            "Ignored if -xy option is used"
+        ),
+    )
+    p.add_argument(
+        "-t", "--twist", action="store_true", help="uses twist option for mge "
+    )
+    p.add_argument(
+        "-c",
+        "--center",
+        action="store_true",
+        help=(
+            "uses the center given in DS9 region file, "
+            "otherwise it will found the x,y peaks within DS9 ellipse"
+        ),
+    )
+    p.add_argument("-p", "--psf", type=float, help="the value of PSF sigma ", default=0)
+    p.add_argument(
+        "-gas",
+        "--gauss",
+        action="store_true",
+        help="uses gauss function for galfit file",
+    )
+    p.add_argument(
+        "-fser",
+        "--freeser",
+        action="store_true",
+        help="leaves the sersic index as a free parameter to fit",
+    )
+    p.add_argument(
+        "-fsk",
+        "--freesky",
+        action="store_true",
+        help="leaves the sky as a free parameter to fit",
+    )
+    p.add_argument(
+        "-ng",
+        "--numgauss",
+        type=int,
+        help="number of gaussians that will be used for galfit.",
+    )
+    p.add_argument(
+        "-xy",
+        "--xypos",
+        nargs=2,
+        type=int,
+        help="provides the (x y) position center of the object to fit",
+    )
+    p.add_argument("-e", "--ellip", type=float, help="ellipticity of object")
+    p.add_argument(
+        "-pa",
+        "--posang",
+        type=float,
+        help="position angle of object. Measured from Y-axis",
+    )
+    return p
+
+
 # check modify: remove initgauss
 def mainMGE():
     """
@@ -12,74 +79,9 @@ def mainMGE():
     This function serves as an example of an API.
     """
     printWelcome()
-    parser = argparse.ArgumentParser(
-        description="fits mge of Cappellari and formats to GALFIT"
-    )
+    parser = _build_parser_mge()
 
-    # parser.add_argument("image", help="the Mask image file to modify or create")
-    parser.add_argument("GalfitFile", help="GALFIT file to obtain the header options")
-    parser.add_argument(
-        "Ds9regFile",
-        help="the DS9 ellipse region file containing the galaxy."
-        + " Ignored if -xy option is used",
-    )
-    parser.add_argument(
-        "-t", "--twist", action="store_true", help="uses twist option for mge "
-    )
-    parser.add_argument(
-        "-c",
-        "--center",
-        action="store_true",
-        help="uses the center given in DS9 region file, "
-        + "otherwise it will found the x,y peaks within DS9 ellipse",
-    )
-    parser.add_argument(
-        "-p", "--psf", type=float, help="the value of PSF sigma ", default=0
-    )
-    parser.add_argument(
-        "-gas",
-        "--gauss",
-        action="store_true",
-        help="uses gauss function for galfit file",
-    )
-
-    parser.add_argument(
-        "-fser",
-        "--freeser",
-        action="store_true",
-        help="leaves the sersic index as a free parameter to fit",
-    )
-    parser.add_argument(
-        "-fsk",
-        "--freesky",
-        action="store_true",
-        help="leaves the sky as a free parameter to fit",
-    )
-
-    parser.add_argument(
-        "-ng",
-        "--numgauss",
-        type=int,
-        help="number of gaussians that will be used for galfit. ",
-    )
-
-    parser.add_argument(
-        "-xy",
-        "--xypos",
-        nargs=2,
-        type=int,
-        help="provides the (x y) position center of the object to fit",
-    )
-
-    parser.add_argument("-e", "--ellip", type=float, help="ellipticity of object")
-    parser.add_argument(
-        "-pa",
-        "--posang",
-        type=float,
-        help="position angle of object. Measured from Y-axis",
-    )
-
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     galfitFile = args.GalfitFile
     regfile = args.Ds9regFile
@@ -117,6 +119,68 @@ def mainMGE():
         ellip=ellip,
         posang=posang,
     )
+    return 0
+
+
+def _build_parser_sbprof() -> argparse.ArgumentParser:
+    p = argparse.ArgumentParser(
+        description="SbProf: creates a surface brightness profile from a ellipse ds9 region"
+    )
+    p.add_argument("Image", help="image fits file")
+    p.add_argument("Ds9Region", help="Ds9 ellipse region file")
+    p.add_argument("-q", "--axrat", type=float, help="axis ratio")
+    p.add_argument(
+        "-pa", "--angle", type=float, help="angular position (same as GALFIT)"
+    )
+    p.add_argument(
+        "-mz", "--mgzpt", type=float, help="Magnitude zero point", default=25
+    )
+    p.add_argument("-m", "--mask", type=str, help="mask fits file")
+    p.add_argument("-s", "--sky", type=float, help="sky value. Default = 0", default=0)
+    p.add_argument("-p", "--plate", type=float, help="plate scale ", default=1)
+    p.add_argument("-o", "--output", type=str, help="output file", default="sb.png")
+    p.add_argument(
+        "-c",
+        "--center",
+        action="store_true",
+        help=(
+            "uses the center given in DS9 region file, "
+            "otherwise it will found the x,y peaks within DS9 ellipse"
+        ),
+    )
+    p.add_argument(
+        "-rx",
+        "--ranx",
+        nargs=2,
+        type=float,
+        help="provide a range for x-axis: xmin - xmax ",
+    )
+    p.add_argument(
+        "-ry",
+        "--rany",
+        nargs=2,
+        type=float,
+        help="provide a range for y-axis: ymin - ymax  ",
+    )
+    p.add_argument(
+        "-lx", "--logx", action="store_true", help="turn the X-axis to logarithm "
+    )
+    p.add_argument(
+        "-px", "--pix", action="store_true", help="turn the top x-axis in pixels "
+    )
+    p.add_argument(
+        "-g", "--grid", action="store_true", help="display a grid in the plot "
+    )
+    p.add_argument(
+        "-r", "--rad", type=float, help="value for a vertical line to add into the plot"
+    )
+    p.add_argument(
+        "-r2",
+        "--rad2",
+        type=float,
+        help="value for a second vertical line to add into the plot",
+    )
+    return p
 
 
 def mainSbProf():
@@ -126,79 +190,17 @@ def mainSbProf():
     """
 
     printWelcome()
-    parser = argparse.ArgumentParser(
-        description="SbProf: creates a surface brightness profile"
-        + " from a ellipse ds9 region"
-    )
 
-    # required arguments
-    parser.add_argument("Image", help="image fits file")
-    parser.add_argument("Ds9Region", help="Ds9 ellipse region file")
-
-    parser.add_argument("-q", "--axrat", type=float, help="axis ratio")
-    parser.add_argument(
-        "-pa", "--angle", type=float, help="angular position (same as GALFIT)"
-    )
-
-    parser.add_argument(
-        "-mz", "--mgzpt", type=float, help="Magnitud zero point", default=25
-    )
-    parser.add_argument("-m", "--mask", type=str, help="mask fits file")
-
-    parser.add_argument(
-        "-s", "--sky", type=float, help="sky value. Default = 0", default=0
-    )
-    parser.add_argument("-p", "--plate", type=float, help="plate scale ", default=1)
-    parser.add_argument(
-        "-o", "--output", type=str, help="output file", default="sb.png"
-    )
-
-    parser.add_argument(
-        "-c",
-        "--center",
-        action="store_true",
-        help="uses the center given in DS9 region file,"
-        + "otherwise it will found the x,y peaks within DS9 ellipse",
-    )
-
-    parser.add_argument(
-        "-rx",
-        "--ranx",
-        nargs=2,
-        type=float,
-        help="provide a range for x-axis: xmin - xmax ",
-    )
-    parser.add_argument(
-        "-ry",
-        "--rany",
-        nargs=2,
-        type=float,
-        help="provide a range for y-axis: ymin - ymax  ",
-    )
-
-    parser.add_argument(
-        "-lx", "--logx", action="store_true", help="turn the X-axis to logarithm "
-    )
-
-    parser.add_argument(
-        "-px", "--pix", action="store_true", help="turn the top x-axis in pixels "
-    )
-    parser.add_argument(
-        "-g", "--grid", action="store_true", help="display a grid in the plot "
-    )
-
-    parser.add_argument(
-        "-r", "--rad", type=float, help="value for a vertical line to add into the plot"
-    )
-    parser.add_argument(
-        "-r2",
-        "--rad2",
-        type=float,
-        help="value for a second vertical line to add into the plot",
-    )
-
-    args = parser.parse_args()
+    parser = _build_parser_sbprof()
+    args = parser.parse_args(argv)
 
     sbProf(args)
 
     print("Done")
+
+    return 0
+
+
+if __name__ == "__main__":
+    # Optional: allow running this module directly
+    sys.exit(mainMGE())  # or choose which to run
