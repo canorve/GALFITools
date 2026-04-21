bfec4e5138d7c3954713764b60a4d40960637316
Christopher Añorve <canorve@gmail.com>
2026-04-20 18:55:45 -0700

    new batch funcion getbarsize

--- src/galfitools/shell/commands_galout.py
+++ src/galfitools/shell/commands_galout.py
@@ -3,7 +3,6 @@ import argparse
 from galfitools.galin.galfit import Galfit
 from galfitools.galout.fitlog2csv import log2csv
 from galfitools.galout.getBarSize import getBarSize
-from galfitools.galout.getBarSize import getMulBarSize
 from galfitools.galout.getCOW import getCOW
 from galfitools.galout.getMissingLight import getMissLight
 from galfitools.galout.getN import getN
@@ -172,61 +171,6 @@ def mainGetBarSize(argv=None) -> int:
     print(f"The bar length is:  \n")
     print(f"  pixels   arcsec ")
     print(f"   {rbar:.2f}     {rbar*plate:.2f}  \n")
-
-    return 0
-
-
-def mainGetMulBarSize(argv=None) -> int:
-    printWelcome()
-    p = argparse.ArgumentParser(
-        description="getBarSize: gets the bar size from Sersic and Ferrer models"
-    )
-    p.add_argument("InputFile", help="file containing a list of file path GALFIT files")
-    p.add_argument(
-        "-d", "--dis", type=int, default=3, help="maximum distance among components"
-    )
-    p.add_argument(
-        "-n",
-        "--numcomp",
-        type=int,
-        default=1,
-        help="number of component to be selected",
-    )
-    p.add_argument(
-        "-o",
-        "--out",
-        type=str,
-        default="barlength.reg",
-        help="output DS9 ellipse region",
-    )
-    p.add_argument(
-        "-co", "--output", type=str, default="barlenghts.csv", help="output csv file"
-    )
-    p.add_argument(
-        "-p",
-        "--plot",
-        action="store_true",
-        help="plots a file of kappa and break radius",
-    )
-    p.add_argument(
-        "-r",
-        "--red",
-        action="store_true",
-        help="If activated, DS9 region ellipse is red",
-    )
-    p.add_argument(
-        "-rx",
-        "--ranx",
-        nargs=2,
-        type=float,
-        help="range of radius to search for barlength",
-    )
-    a = p.parse_args(argv)
-
-    ret = getMulBarSize(
-        a.InputFile, a.dis, a.numcomp, a.plot, a.ranx, a.out, a.red, a.output
-    )
-    print("done ")
 
     return 0
 
