29ce6b113419a42914ec9e889f02ae204580865c
Christopher Añorve <canorve@gmail.com>
2026-07-04 15:21:06 -0700

    changing constraints for bulge in getSersic

--- src/galfitools/galin/getSersic.py
+++ src/galfitools/galin/getSersic.py
@@ -298,10 +298,10 @@ def getSersic(
 
                 if consbulge:
 
-                    print("# 1    q  0.75 to 1  ")
+                    print("# 1    q  0.5 to 1  ")
                     print("# 1    n  0.1 to 10  ")
                     print("# 2    q  0 to 0.65 ")
-                    constlinebulge = " 1   q  0.75 to 1 \n"
+                    constlinebulge = " 1   q  0.5 to 1 \n"
                     constlinesersic = " 1   n  0.1 to 10 \n"
                     constlinebar = " 2   q  0 to 0.65 \n"
                     fout.write(constlinebulge)
@@ -320,9 +320,9 @@ def getSersic(
 
                 if consbulge:
 
-                    print("# 1    q  0.75 to 1  ")
+                    print("# 1    q  0.5 to 1  ")
                     print("# 1    n  0.1 to 10  ")
-                    constlinebulge = " 1   q  0.75 to 1 \n"
+                    constlinebulge = " 1   q  0.5 to 1 \n"
                     constlinesersic = " 1   n  0.1 to 10 \n"
                     fout.write(constlinebulge)
                     fout.write(constlinesersic)
